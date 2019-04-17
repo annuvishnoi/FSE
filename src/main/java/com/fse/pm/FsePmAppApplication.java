bc3d8764eafa7bb173619cac1406b925b73c5e34
annuvishnoi <annu.vishnoi@gmail.com>
2019-04-18 02:14:16 +0530

    initial commit..

---
+++ src/main/java/com/fse/pm/FsePmAppApplication.java
@@ -0,0 +1,13 @@
+package com.fse.pm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsePmAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsePmAppApplication.class, args);
+	}
+
+}
